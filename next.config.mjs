c303a6b17955f31e2a22e5264f374c66d5dca84c
s-hanzala <s.hanzala@i-diligence.com>
2025-01-23 19:18:45 +0530

    Spelling change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   exportTrailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
